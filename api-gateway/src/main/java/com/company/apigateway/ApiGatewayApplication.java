f7838458ad4a2f6e30c55c69322e9d2793b0b486
ulugbek9909 <ulugbekkhan94@mail.ru>
2023-03-06 12:33:27 +0500

    Oauth2 added

---
+++ api-gateway/src/main/java/com/company/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.company.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Author : Khonimov Ulugbek
+ * Date : 2/8/2023
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
--- order-service/src/main/java/com/company/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/company/orderservice/config/WebClientConfig.java
@@ -3,6 +3,7 @@ package com.company.orderservice.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 /**
@@ -18,5 +19,11 @@ public class WebClientConfig {
     @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
